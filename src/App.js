f5914d33907a1f5573b070f74773d4e93f883167
Gisele-123 <giselemigisha53@gmail.com>
2023-04-28 08:33:50 +0200

    Added stre and provider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import GridBoard from './Components/GridBoard';
 import NextBlock from './Components/NextBlock';
 import ScoreBoard from './Components/ScoreBoard';
 import  Controls from './Components/Controls'
 import MessagePopup from './Components/MessagePopup';
+import reducers from './Reducers';
+
+const store=createStore(reducers)
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
       <header className="App-header">
        <h1 className='App-Title'>Tetris Arcade game</h1>
@@ -18,6 +24,7 @@ function App() {
       <Controls />
       <MessagePopup />
     </div>
+    </Provider>
   );
 }
 
