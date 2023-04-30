bb140d509e90f9f81df3f7cfb4a856c8fcbaf600
Gisele-123 <giselemigisha53@gmail.com>
2023-04-30 10:00:49 +0200

    Added connection for controls

--- src/Components/Controls.js
+++ src/Components/Controls.js
@@ -1,19 +1,25 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { moveDown, moveLeft, moveRight, rotate} from '../Actions'
 
 export default function Controls(props){
+    const dispatch=useDispatch()
+    const isRunning=useSelector((state)=>state.isRunning)
+
+
     return (
         <div className="controls">
             <button className="control-button" onClick={(e)=>{
-
+                dispatch(moveLeft())
             }}>left</button>
               <button className="control-button" onClick={(e)=>{
-
+                dispatch(moveRight())
             }}>Right</button>
   <button className="control-button" onClick={(e)=>{
-
+                dispatch(rotate())
             }}>Rotate</button>
   <button className="control-button" onClick={(e)=>{
-
+                dispatch(moveDown())
             }}>Down</button>
         </div>
     )
--- src/Components/GridBoard.js
+++ src/Components/GridBoard.js
@@ -25,12 +25,6 @@ export default function GridBoard(props){
             />
         })
     })
-    // for(let row =0; row<18 ; row++){
-    //     grid.push([])
-    //     for(let col=0; col<10 ;col++){
-    //         grid[row].push(<GridSquare key={`${col}${row}`}/>)
-    //     }
-    // }
      return (
         <div className='grid-board'>
              {gridSquares}
